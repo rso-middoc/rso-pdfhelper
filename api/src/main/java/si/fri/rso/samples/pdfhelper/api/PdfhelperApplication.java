24489b6c63608a6bec341cb3e4a0e3c5c95ec910
iztok <ir8617@student.uni-lj.si>
2020-01-09 12:20:17 +0100

    discovery

--- api/src/main/java/si/fri/rso/samples/pdfhelper/api/PdfhelperApplication.java
+++ api/src/main/java/si/fri/rso/samples/pdfhelper/api/PdfhelperApplication.java
@@ -3,6 +3,9 @@ package si.fri.rso.samples.pdfhelper.api;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
+
+@RegisterService
 @ApplicationPath("v1")
 public class PdfhelperApplication extends Application {
 }
